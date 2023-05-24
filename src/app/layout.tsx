a916b477811d6cf27274ba1792c1afaa0f015bbb
Sean O'Connor <so@seano.dev>
2023-05-24 17:26:43 -0600

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: "Sean O'Connor"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={classNames(inter.className)}>{children}</body>
+    </html>
+  );
+}
