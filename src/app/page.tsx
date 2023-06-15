33c8196944d9dd510595ef05ec4bfa2afe3fda97
Sean O'Connor <so@seano.dev>
2023-06-15 16:57:27 -0600

    Update home page
    Styling, formatting, and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,39 @@ import { FaGithub, FaLinkedin, FaMastodon, FaPaperPlane } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <main className="container min-h-screen flex flex-col justify-center items-center md:items-start gap-8 p-8">
-      <h1 className="text-6xl font-black text-center md:text-8xl md:text-left">
-        Hey, I&apos;m Sean.
-      </h1>
-      <p className="text-3xl font-semibold text-center md:text-4xl md:text-left">
-        I&apos;m a Denver-based full-stack software developer.
-      </p>
-      <div className="flex gap-8">
-      <IconButton
-          href="https://www.linkedin.com/in/seaneoo"
-          icon={<FaLinkedin className="w-full h-full"  />}
-          label="my linkedin"
-        />
-        <IconButton
-          href="https://github.com/seaneoo"
-          icon={<FaGithub className="w-full h-full" />}
-          label="my github"
-        />
-        <IconButton
-          href="https://mastodon.social/@seaneoo"
-          icon={<FaMastodon className="w-full h-full" />}
-          label="my mastodon"
-        />
-        <IconButton
-          href="mailto:so@seano.dev"
-          icon={<FaPaperPlane className="w-full h-full" />}
-          label="my email"
-        />
-      </div>
+    <main className="container flex min-h-screen flex-col items-center justify-center gap-16 p-8">
+      <section>
+        <h1 className="mb-4 text-center text-5xl font-black text-brand md:text-6xl">
+          Hey, I&apos;m Sean O&apos;Connor.
+        </h1>
+        <p className="text-center text-3xl font-semibold md:text-4xl">
+          I&apos;m a Denver-based full stack software developer.
+        </p>
+      </section>
+      <section>
+        <div className="flex gap-8">
+          <IconButton
+            href="https://www.linkedin.com/in/seaneoo"
+            icon={<FaLinkedin className="h-full w-full" />}
+            label="my linkedin"
+          />
+          <IconButton
+            href="https://github.com/seaneoo"
+            icon={<FaGithub className="h-full w-full" />}
+            label="my github"
+          />
+          <IconButton
+            href="https://mastodon.social/@seaneoo"
+            icon={<FaMastodon className="h-full w-full" />}
+            label="my mastodon"
+          />
+          <IconButton
+            href="mailto:so@seano.dev"
+            icon={<FaPaperPlane className="h-full w-full" />}
+            label="my email"
+          />
+        </div>
+      </section>
     </main>
   );
 }
