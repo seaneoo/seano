efef9e4586c6f256edab4fbd81b16f478098ec0c
Sean O'Connor <so@seano.dev>
2023-06-23 16:00:57 -0600

    Create pages /about and /work

---
+++ src/app/work/page.tsx
@@ -0,0 +1,9 @@
+import Header from '@/components/header';
+
+export default function Work() {
+  return (
+    <main className="container relative flex min-h-screen flex-col items-center justify-center gap-16 p-8">
+      <Header />
+    </main>
+  );
+}
