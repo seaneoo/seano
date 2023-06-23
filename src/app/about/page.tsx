7346a200cc4038bc12b5e3154e9d993debf0daac
Sean O'Connor <so@seano.dev>
2023-06-23 17:11:30 -0600

    Update pages /about and /work

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,50 @@
+import ExtLink from '@/components/ext-link';
 import Header from '@/components/header';
+
+const emailGreetings = ['Hello!', 'Hey!', 'Hi!'];
 
 export default function About() {
   return (
     <main className="container relative flex min-h-screen flex-col items-center justify-center gap-16 p-8">
       <Header />
+
+      <section className="w-full text-center text-xl md:w-5/6 lg:w-3/4 lg:text-2xl xl:w-1/2">
+        <p className="mb-4">
+          Hey, my name is Sean. I&apos;m a <mark>Denver-based</mark> full stack
+          software developer specializing in front end web development & design
+          using <mark>React</mark> and <mark>Next.js</mark>.
+        </p>
+
+        <p className="mb-4">
+          I also love to play around with iOS app development and back end
+          development—with the former being my future career plan.
+        </p>
+
+        <p className="mb-8">
+          I would love to hear from you! Please{' '}
+          <mark>
+            <ExtLink
+              href={`mailto:so@seano.dev?subject=${
+                emailGreetings[
+                  Math.floor(Math.random() * emailGreetings.length)
+                ]
+              }`}>
+              send me an email
+            </ExtLink>
+          </mark>
+          .
+        </p>
+
+        <p className="text-lg">
+          (
+          <mark>
+            <ExtLink href="https://files.catbox.moe/sgctmq.jpeg">
+              Obligatory cat picture
+            </ExtLink>
+          </mark>
+          )
+        </p>
+      </section>
     </main>
   );
 }
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,9 +1,20 @@
+import ExtLink from '@/components/ext-link';
 import Header from '@/components/header';
 
 export default function Work() {
   return (
     <main className="container relative flex min-h-screen flex-col items-center justify-center gap-16 p-8">
       <Header />
+
+      <p className="text-center text-xl lg:text-2xl">
+        Coming soon!
+        <br />
+        In the meantime, check out my{' '}
+        <mark>
+          <ExtLink href="https://github.com/seaneoo/">GitHub</ExtLink>
+        </mark>
+        . ╰(*°▽°*)╯
+      </p>
     </main>
   );
 }
