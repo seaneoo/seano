8c519705b0228863938bc3d735262f6fc97cc571
Sean O'Connor <so@seano.dev>
2023-07-05 16:01:14 -0600

    Update props for IconButton

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,16 +1,15 @@
 'use client';
 
+import Link from 'next/link';
 import ExtLink from './ext-link';
 
-type Props = {
-  href: string;
+type Props = React.ComponentProps<typeof Link> & {
   icon?: React.ReactNode;
-  label: string;
 };
 
-export default function IconButton({ href, icon, label }: Props) {
+export default function IconButton({ icon, ...props }: Props) {
   return (
-    <ExtLink href={href} aria-label={label} className="text-text no-underline">
+    <ExtLink {...props} className="text-text no-underline">
       <span className="block h-8 w-8">{icon}</span>
     </ExtLink>
   );
